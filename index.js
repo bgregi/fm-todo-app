3772d476acd27e048d3efeb44bc08aad79dd8f99
Bruno Geraldi <brunogeraldieng@gmail.com>
2021-08-24 21:45:49 -0300

    JS done

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const input = document.getElementById("create-input")
 
-let todoList = [{
-    item: "hey",
-    isChecked: true
-}]
+let todoList = []
 
 function registerItem() {
     const item = input.value
@@ -20,14 +17,22 @@ function registerItem() {
     }
 
     todoList.push(todoItem)
-    
-    console.log(todoItem)
-    console.log(todoList)
-    
+
     input.value = ""
-    
+
     listItems(todoList)
 }
+const createButton = document.getElementById("create-button")
+createButton.addEventListener("click", registerItem)
+
+// Runs registerItem function when Enter key is released
+input.addEventListener("keyup", function (event) {
+    event.preventDefault()
+    if (event.keyCode === 13) {
+        createButton.click()
+    }
+}
+)
 
 function listItems(list) {
     let divTodoList = document.getElementById("todo-list")
@@ -37,36 +42,35 @@ function listItems(list) {
         divTodoList.removeChild(divTodoList.firstChild)
     }
 
-    console.log(divTodoList.childNodes)
-    console.log(typeof(divTodoList.childNodes))
-
     //Reconstructs all nodes with provided array
     for (i of list) {
 
         let divTodoItem = document.createElement("div")
         divTodoItem.className = "todo-item"
-        
+
         let divTodoItemMain = document.createElement("div")
         divTodoItemMain.className = "todo-item-main"
 
         let divCheckBox = document.createElement("div")
-        divCheckBox.className = "checkbox"
-                
+        divCheckBox.classList.add("checkbox")
+        divCheckBox.classList.add("checkbox-list")
+        divCheckBox.id = "c-" + list.indexOf(i)
+
         let imgCheck = document.createElement("img")
         imgCheck.src = "images/icon-check.svg"
         imgCheck.alt = "Check"
-        
+        imgCheck.id = "i-" + list.indexOf(i)
+
         let pItem = document.createElement("p")
 
         let text = document.createTextNode(i.item)
 
-        // console.log(list)
-        
         let imgCross = document.createElement("img")
         imgCross.src = "./images/icon-cross.svg"
         imgCross.alt = "Delete"
         imgCross.className = "delete-button"
-        
+        imgCross.id = "d-" + list.indexOf(i)
+
         if (i.isChecked == true) {
             divCheckBox.classList.add("checkbox-checked")
             pItem.className = "todo-item-checked"
@@ -93,7 +97,7 @@ function listItems(list) {
 function itemsLeft(list) {
     //Filters checked items in the list
     let onlyUnchecked = list.filter(i => i.isChecked == false)
-    
+
     let divBottomInfo = document.getElementById("bottom-info")
 
     //Clears existing span
@@ -109,16 +113,76 @@ function itemsLeft(list) {
 
 }
 
+//Gets id of delete button when clicked (used in deleteItem function)
+document.addEventListener('click', function(e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement,
+        text = target.textContent || target.innerText;
+    
 
+    // let deleteIndex = parseInt(target.id)
 
-const createButton = document.getElementById("create-button")
-createButton.addEventListener("click", registerItem)
-
-// Runs registerItem function when Enter key is released
-input.addEventListener("keyup", function (event) {
-    event.preventDefault()
-    if (event.keyCode === 13) {
-        createButton.click()
+    if (target.className == "delete-button") {
+        deleteItem(target)
     }
+
+    if (target.className == "checkbox-list" || target.alt == "Check") {
+        checkItem(target)
+    }
+
+}, false);
+
+function deleteItem(target) {
+    let deleteIndex = parseInt(target.id.split("-").pop())
+
+    todoList.splice(deleteIndex, 1)
+
+    listItems(todoList)
 }
-)
+
+function checkItem(target) {
+    let deleteIndex = parseInt(target.id.split("-").pop())
+
+    let itemIsChecked = todoList[deleteIndex].isChecked
+
+    if (itemIsChecked) {
+        todoList[deleteIndex].isChecked = false
+    } else {
+        todoList[deleteIndex].isChecked = true
+    }
+
+    listItems(todoList)
+}
+
+function clearCompleted() {
+    let onlyUnchecked = todoList.filter(i => i.isChecked == false)
+
+    // console.log(todoList)
+    todoList = onlyUnchecked
+
+    listItems(onlyUnchecked)
+}
+let clearButton = document.getElementById("clear-button")
+clearButton.addEventListener("click", clearCompleted)
+
+function filterAll() {
+    listItems(todoList)
+}
+let allButton = document.getElementById("filter-all")
+allButton.addEventListener("click", filterAll)
+
+function filterActive() {
+    let onlyActive = todoList.filter(i => i.isChecked == false)
+
+    listItems(onlyActive)
+}
+let activeButton = document.getElementById("filter-active")
+activeButton.addEventListener("click", filterActive)
+
+function filterCompleted() {
+    let onlyCompleted = todoList.filter(i => i.isChecked == true)
+
+    listItems(onlyCompleted)
+}
+let completedButton = document.getElementById("filter-completed")
+completedButton.addEventListener("click", filterCompleted)
